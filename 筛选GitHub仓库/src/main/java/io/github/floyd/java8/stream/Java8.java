b37ab36035e0c32194b12979e6195cb1a43a8279
790013438 <790013438@qq.com>
2018-07-06 13:45:24 +0800

    init

---
+++ 筛选GitHub仓库/src/main/java/io/github/floyd/java8/stream/Java8.java
@@ -0,0 +1,9 @@
+package io.github.floyd.java8.stream;
+
+public class Java8 {
+
+    public static void main(String[] args) {
+        List<Project> projects = Project.buildData();
+        List<String> names = projects.stream(
+    }
+}
