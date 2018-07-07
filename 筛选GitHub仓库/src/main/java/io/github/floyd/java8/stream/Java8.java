cf6f89eda2d45b498154adca66054ee60ac9816d
790013438 <790013438@qq.com>
2018-07-07 09:03:26 +0800

    stream，会将多个操作合成一个
    forEach 遍历，很多操作不执行，直到collect
    
    Blade
    Blade
    Tale
    Tale
    Vue.js
    Vue.js
    [Blade, Tale, Vue.js]

--- 筛选GitHub仓库/src/main/java/io/github/floyd/java8/stream/Java8.java
+++ 筛选GitHub仓库/src/main/java/io/github/floyd/java8/stream/Java8.java
@@ -11,8 +11,14 @@ public class Java8 {
     public static void main(String[] args) {
         List<Project> projects = FilterProjects.getProjects();
         List<String> names = projects.stream()
-                .filter(p -> p.getStars() > 1000)
-                .map(Project::getName)
+                .filter(p -> {
+                    System.out.println(p.getName());
+                    return p.getStars() > 1000;
+                })
+                .map(p -> {
+                    System.out.println(p.getName());
+                    return p.getName();
+                })
                 .limit(3)
                 .collect(Collectors.toList());
         System.out.println(names);
