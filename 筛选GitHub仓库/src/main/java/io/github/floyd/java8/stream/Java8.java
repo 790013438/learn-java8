aece264da0b0b5f36a885721d2752bf87f2370b5
790013438 <790013438@qq.com>
2018-07-07 08:46:21 +0800

    stream

--- 筛选GitHub仓库/src/main/java/io/github/floyd/java8/stream/Java8.java
+++ 筛选GitHub仓库/src/main/java/io/github/floyd/java8/stream/Java8.java
@@ -1,9 +1,20 @@
 package io.github.floyd.java8.stream;
 
+import io.github.floyd.java8.lambda.FilterProjects;
+import io.github.floyd.java8.lambda.domain.Project;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Java8 {
 
     public static void main(String[] args) {
-        List<Project> projects = Project.buildData();
-        List<String> names = projects.stream(
+        List<Project> projects = FilterProjects.getProjects();
+        List<String> names = projects.stream()
+                .filter(p -> p.getStars() > 1000)
+                .map(Project::getName)
+                .limit(3)
+                .collect(Collectors.toList());
+        System.out.println(names);
     }
 }
