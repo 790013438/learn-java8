6ddeeb0f043729cc9fae7d718c47cfa58e3be669
790013438 <790013438@qq.com>
2018-07-10 00:23:54 +0800

    stream .distinct(), 去重

--- 筛选GitHub仓库/src/main/java/io/github/floyd/java8/stream/demo/FilterStream.java
+++ 筛选GitHub仓库/src/main/java/io/github/floyd/java8/stream/demo/FilterStream.java
@@ -5,6 +5,7 @@ import io.github.floyd.java8.lambda.domain.Project;
 
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FilterStream {
 
@@ -14,5 +15,10 @@ public class FilterStream {
             .filter(project -> project.getStars() > 1000)
             .collect(Collectors.toList());
         System.out.println(projectsCollect);
+
+        Stream<Integer> numbers = Stream.of(1, 2, 3, 3, 2, 4);
+        numbers.distinct()
+            .forEach(System.out::println);
     }
+
 }
