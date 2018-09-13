200eb81bd6b2045be527ab8dda1ad2fb18547ef3
790013438 <790013438@qq.com>
2018-09-13 15:29:50 +0800

    列出所有的隐藏文件

--- src/main/java/io/github/floyd/Main.java
+++ src/main/java/io/github/floyd/Main.java
@@ -1,8 +1,19 @@
 package io.github.floyd;
 
+import java.io.File;
+import java.io.FileFilter;
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        File[] hiddenFiles = new File(".").listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return file.isHidden();
+            }
+        });
+        Arrays.stream(hiddenFiles)
+                .forEach(System.out::println);
     }
 }
