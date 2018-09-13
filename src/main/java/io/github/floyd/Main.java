8a609617c18796f4ce82cd2dc415c4690b6c9cb0
790013438 <790013438@qq.com>
2018-09-13 16:08:24 +0800

    递归打印隐藏文件

--- src/main/java/io/github/floyd/Main.java
+++ src/main/java/io/github/floyd/Main.java
@@ -1,10 +1,18 @@
 package io.github.floyd;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.File;
 import java.io.FileFilter;
 import java.util.Arrays;
 
+/**
+ * @author 程淼
+ */
 public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
         File[] hiddenFiles = new File(".").listFiles(new FileFilter() {
@@ -15,5 +23,25 @@ public class Main {
         });
         Arrays.stream(hiddenFiles)
                 .forEach(System.out::println);
+
+        recursive(new File("."));
+    }
+
+    private static void recursive (File file) {
+        if (file.isDirectory()) {
+            File[] hiddenFiles = file.listFiles(new FileFilter() {
+                @Override
+                public boolean accept(File pathname) {
+                    return pathname.isHidden();
+                }
+            });
+            Arrays.stream(hiddenFiles != null ? hiddenFiles : new File[0])
+                    .forEach(e -> {
+                        if (e.isDirectory()) {
+                            recursive(e);
+                        }
+                        logger.debug(e.toString());
+                    });
+        }
     }
 }
