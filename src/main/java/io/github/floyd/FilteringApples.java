2851bc7f13c489ad51f9626f9eca448d3fee3dac
790013438 <790013438@qq.com>
2018-09-20 16:32:28 +0800

    stream,filter,toList

---
+++ src/main/java/io/github/floyd/FilteringApples.java
@@ -0,0 +1,65 @@
+package io.github.floyd;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+/**
+ * @author floyd
+ */
+public class FilteringApples {
+
+    private static final Logger logger = LoggerFactory.getLogger(FilteringApples.class);
+
+    public static void main(String ...args) {
+
+        List<Apple> inventory = Arrays.asList(
+                new Apple(80, "green"),
+                new Apple(155, "green"),
+                new Apple(120, "red"));
+
+        List<Apple> heavyApples = inventory.stream()
+            .filter((Apple a) -> a.getWeight() > 150)
+            .collect(toList());
+
+        logger.debug("heavy apples {}", heavyApples);
+    }
+
+    public static class Apple {
+        private int weight;
+        private String color;
+
+        Apple(int weight, String color){
+            this.weight = weight;
+            this.color = color;
+        }
+
+        Integer getWeight() {
+            return weight;
+        }
+
+        public void setWeight(Integer weight) {
+            this.weight = weight;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setColor(String color) {
+            this.color = color;
+        }
+
+        @Override
+        public String toString() {
+            return "Apple{" +
+                "color='" + color + '\'' +
+                ", weight=" + weight +
+                '}';
+        }
+    }
+}
--- src/main/java/io/github/floyd/Main.java
+++ src/main/java/io/github/floyd/Main.java
@@ -4,7 +4,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.io.FileFilter;
 import java.util.Arrays;
 
 /**
