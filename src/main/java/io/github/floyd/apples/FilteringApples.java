5819fc778cca2c49235034db586bb834b7cccfaf
790013438 <790013438@qq.com>
2018-09-21 14:14:22 +0800

    以函数为参数变量

--- src/main/java/io/github/floyd/apples/FilteringApples.java
+++ src/main/java/io/github/floyd/apples/FilteringApples.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 
 import static java.util.stream.Collectors.toList;
 
@@ -22,15 +23,36 @@ public class FilteringApples {
                 new Apple(155, "green"),
                 new Apple(120, "red"));
 
+        // 苹果重量
         List<Apple> heavyApples = inventory.stream()
             .filter((Apple a) -> a.getWeight() > 150)
             .collect(toList());
         logger.debug("heavy apples {}", heavyApples);
 
+        // 苹果重量
         List<Apple> heavyApples2 = inventory.parallelStream()
             .filter((Apple a) -> a.getWeight() > 150)
             .collect(toList());
         logger.debug("heavy apples {}", heavyApples2);
+
+        // 打印苹果重量
+        prettyPrintApple(inventory, apple -> "苹果重量：" + apple.getWeight().toString());
+
+        // 打印每个苹果并提及是重还是轻
+        prettyPrintApple(inventory, apple -> {
+            int heavyApple = 150;
+            if (apple.getWeight() >= heavyApple) {
+                return "重苹果 :" + apple.toString();
+            }
+            return apple.toString();
+        });
+    }
+
+    public static void prettyPrintApple(List<Apple> inventory, Function<Apple, String> print) {
+        for (Apple apple : inventory) {
+          String output = print.apply(apple);
+          logger.debug(output);
+        }
     }
 
     public static class Apple {
