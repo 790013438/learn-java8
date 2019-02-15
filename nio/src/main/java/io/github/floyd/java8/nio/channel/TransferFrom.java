d27c0b4893265343189c4e23d4d34f0466ed34b2
790013438 <790013438@qq.com>
2019-02-15 15:44:59 +0800

    jar文件位置的父路径下的配置文件，之前是一直在jar文件里获得

--- nio/src/main/java/io/github/floyd/java8/nio/channel/TransferFrom.java
+++ nio/src/main/java/io/github/floyd/java8/nio/channel/TransferFrom.java
@@ -24,21 +24,24 @@ public class TransferFrom {
 
         // 查找jar的旁边有没有文件
         CodeSource codeSource = TransferFrom.class.getProtectionDomain().getCodeSource();
-        String location = codeSource.getLocation().getFile();
+        File location = new File(codeSource.getLocation().getFile());
         LOGGER.debug("jar或class所在的路径{}", location);
-        File srcFile = new File(location, FILE);
+        File srcFile = new File(location.getParentFile(), FILE);
+        // 包含到一个类里
+        SrcFile srcFile1 = new SrcFile();
+        srcFile1.setFile(srcFile);
 
 
         // 写入copy.txt
         String toFileString = "copy.txt";
-        try (ReadableByteChannel readableByteChannel = readableByteChannelFunction.apply(srcFile);
+        try (ReadableByteChannel readableByteChannel = readableByteChannelFunction.apply(srcFile1);
              // 要写入的文件, 位置在执行命令窗口所在的位置
              FileChannel fileChannel = new RandomAccessFile(toFileString, "rw").getChannel();
              // 比较使用简便的方式，放在jar旁边
-             FileChannel transferFileChannel = new RandomAccessFile(new File(location, "nio-data.md"), "rw").getChannel()) {
+             FileChannel transferFileChannel = new RandomAccessFile(new File(location.getParentFile(), "nio-data.md"), "rw").getChannel()) {
 
-            // 使用简单的
-            transferFileChannel.transferFrom(readableByteChannel, 0, srcFile.length());
+            // 使用简单的, 如果jar包旁边的文件存在，则是srcFile.length()；如果不存在，则是jar包里的默认资源inputStream.available()
+            transferFileChannel.transferFrom(readableByteChannel, 0, srcFile1.getSize());
 
             // 缓存大小
             ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
@@ -60,17 +63,21 @@ public class TransferFrom {
         }
     }
 
-    private static ThrowableFunction<File, ReadableByteChannel> readableByteChannelFunction = (File srcFile) -> {
+    private static ThrowableFunction<SrcFile, ReadableByteChannel> readableByteChannelFunction = (SrcFile srcFile) -> {
+        LOGGER.debug("{}文件存在: {}", srcFile, srcFile.getFile().exists());
         // 有，jar包旁边的文件作为资源文件，源文件
-        if (srcFile.exists()) {
-            RandomAccessFile srcRandomAccessFile = new RandomAccessFile(srcFile, "rw");
+        if (srcFile.getFile().exists()) {
+            RandomAccessFile srcRandomAccessFile = new RandomAccessFile(srcFile.getFile(), "rw");
+            srcFile.setSize(srcFile.getFile().length());
             return srcRandomAccessFile.getChannel();
         }
         // 没有，则从jar包里获取源文件
         // 得到jar包的文件
         InputStream inputStream = TransferFrom.class.getClassLoader().getResourceAsStream(FILE);
         assert inputStream != null;
-        LOGGER.debug("inputStream 的大小{}", inputStream.available());
+        LOGGER.debug("inputStream{} 的大小: {}", inputStream, inputStream.available());
+        // todo 安全问题，会截断吗，available获得的是int
+        srcFile.setSize(new Long(inputStream.available()));
         // 源文件
         return Channels.newChannel(inputStream);
     };
@@ -85,5 +92,26 @@ public class TransferFrom {
          * @throws Exception 异常
          */
         R apply(T t) throws Exception;
+    }
+
+    private static class SrcFile{
+        private Long size;
+        private File file;
+
+        public Long getSize() {
+            return size;
+        }
+
+        public void setSize(Long size) {
+            this.size = size;
+        }
+
+        public File getFile() {
+            return file;
+        }
+
+        public void setFile(File file) {
+            this.file = file;
+        }
     }
 }
