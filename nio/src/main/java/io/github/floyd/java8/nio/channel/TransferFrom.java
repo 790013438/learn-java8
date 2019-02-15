3375e4a12804cff205cb586f6633212bf51a06a1
790013438 <790013438@qq.com>
2019-02-15 09:59:54 +0800

    从jar文件里拷贝出文件放在shell里执行命令的位置
    执行java -jar 的cmd的位置

--- nio/src/main/java/io/github/floyd/java8/nio/channel/TransferFrom.java
+++ nio/src/main/java/io/github/floyd/java8/nio/channel/TransferFrom.java
@@ -5,41 +5,78 @@ import org.slf4j.LoggerFactory;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.RandomAccessFile;
-import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
+import java.nio.channels.ReadableByteChannel;
+import java.security.CodeSource;
 
 /**
  * @author 79001
  */
 public class TransferFrom {
     private static final Logger LOGGER = LoggerFactory.getLogger(TransferFrom.class);
+    private static final String FILE = "log4j.properties";
 
     public static void main(String... args) throws IOException {
-        String filePath;
-        URL url = TransferFrom.class.getClassLoader().getResource(".");
-        LOGGER.debug("URL {}", url);
-        if (url != null) {
-            filePath = url.getFile();
-        } else {
-            filePath = new File("").getAbsolutePath();
-        }
-        LOGGER.debug("获取到的相对路径{}", filePath);
-        String file = filePath + File.separator + "log4j.properties";
-        String toFile = filePath + File.separator + "copy.txt";
 
-        try (RandomAccessFile fromRandomAccessFile = new RandomAccessFile(file, "rw");
-             RandomAccessFile toRandomAccessFile = new RandomAccessFile(toFile, "rw")) {
-            LOGGER.debug("要读取的文件{}", file);
-            FileChannel fromFileChannel = fromRandomAccessFile.getChannel();
-            FileChannel toFileChannel = toRandomAccessFile.getChannel();
+        // 查找jar的旁边有没有文件
+        CodeSource codeSource = TransferFrom.class.getProtectionDomain().getCodeSource();
+        String location = codeSource.getLocation().getFile();
+        File srcFile = new File(location, FILE);
 
-            long position = 0;
-            long count = fromFileChannel.size();
 
-            toFileChannel.transferFrom(fromFileChannel, position, count);
+        // 写入copy.txt
+        String toFileString = "copy.txt";
+        try (ReadableByteChannel readableByteChannel = readableByteChannelFunction.apply(srcFile);
+             // 要写入的文件
+             FileChannel fileChannel = new RandomAccessFile(toFileString, "rw").getChannel()) {
+            // 缓存大小
+            ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+            // 读取文件内容到缓存
+            int readInt = readableByteChannel.read(byteBuffer);
+            // 当缓存里有内容，没有读取完
+            while (readInt > 0) {
+                // 将缓存里的转为写入文件
+                byteBuffer.flip();
+                // 写入到目标文件
+                fileChannel.write(byteBuffer);
+                // 去掉已经读取的缓存
+                byteBuffer.compact();
+                // 读取文件内容到缓存
+                readInt = readableByteChannel.read(byteBuffer);
+            }
         } catch (Exception e) {
-            LOGGER.error("复制失败");
+            LOGGER.error("读取文件{}", e);
         }
+    }
+
+    private static ThrowableFunction<File, ReadableByteChannel> readableByteChannelFunction = (File srcFile) -> {
+        // 有，jar包旁边的文件作为资源文件，源文件
+        if (srcFile.exists()) {
+            RandomAccessFile srcRandomAccessFile = new RandomAccessFile(srcFile, "rw");
+            return srcRandomAccessFile.getChannel();
+        }
+        // 没有，则从jar包里获取源文件
+        // 得到jar包的文件
+        InputStream inputStream = TransferFrom.class.getClassLoader().getResourceAsStream(FILE);
+        assert inputStream != null;
+        LOGGER.debug("inputStream 的大小{}", inputStream.available());
+        // 源文件
+        return Channels.newChannel(inputStream);
+    };
+
+    @FunctionalInterface
+    private interface ThrowableFunction<T, R> {
+        /**
+         * 会抛出异常的function
+         *
+         * @param t 给参数
+         * @return 转换后的结果
+         * @throws Exception 异常
+         */
+        R apply(T t) throws Exception;
     }
 }
