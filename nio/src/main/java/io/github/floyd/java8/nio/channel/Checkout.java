23123c9f238bdeb5eacfee1f10543f4fe2a6252b
790013438 <790013438@qq.com>
2019-05-14 15:16:51 +0800

    使用java反射

--- nio/src/main/java/io/github/floyd/java8/nio/channel/Checkout.java
+++ nio/src/main/java/io/github/floyd/java8/nio/channel/Checkout.java
@@ -1,5 +1,9 @@
 package io.github.floyd.java8.nio.channel;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.function.Supplier;
+
 /**
  * @author 程淼
  * @version 1.0
@@ -9,9 +13,13 @@ package io.github.floyd.java8.nio.channel;
  * date 2019/2/25
  */
 public class Checkout {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
         CheckInterface check2 = new Check2Impl();
         check2.print();
         ((Check2Impl) check2).getLogger().debug(check2.supply());
+        Class reflectClazz = Class.forName("io.github.floyd.java8.nio.overload.ReflectRepository");
+        Method method = reflectClazz.getDeclaredMethod("haHa", java.util.function.Supplier.class);
+        method.setAccessible(true);
+        method.invoke(reflectClazz.newInstance(), (Supplier<String>)() -> "调用成功吗");
     }
 }
